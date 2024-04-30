1c63fc4d418893b38ecb958dc909f2afcb7dd81b
blueapple825 <blueapple8259@gmail.com>
2024-05-01 00:41:39 +0900

    테스트용 코드 추가

--- lz77.py
+++ lz77.py
@@ -1,5 +1,9 @@
 def compression(text):
     pass
 
-if __name__=="__main__":
-    pass
+if __name__ == "__main__":
+    inputText = input("text: ").encode("utf-8")
+    result=compression(inputText)
+
+    print("결과: ", result)
+    print("압축률: ", len(result) / len(inputText))
